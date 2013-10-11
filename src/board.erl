75d119bf46fe84ae9fa8c3405527165c3218060a
Vipin Nair <swvist@gmail.com>
2013-10-12 02:52:56 +0530

    Reversi Board Implementations. (Sorry, for the large commit)
    1) Find valid line moves for a board cell.
    2) Find valid line moves for a particular color.
    3) Find valid moves by merging line moves and eliminating redundency.
    4) Really Dumb AI. Find all valid moves, pick the first one, pray, move one. (One day this will be better. Features like cell weightage, minimax with Alpha-Beta Pruning and ProbCut will be implemented.

--- src/board.erl
+++ src/board.erl
@@ -15,14 +15,161 @@ get(X, Y, Board) ->
 	array:get(Y, array:get(X, Board)).
 
 create_default_board() ->
-	Board = create_empty_board(),
-	B1 = set(3, 3, w, Board),
-	B2 = set(3, 4, b, B1),
-	B3 = set(4, 3, b, B2),
-	B4 = set(4, 4, w, B3),
-	B4.
+	set(3, 3, w,
+		set(3, 4, b,
+			set(4, 3, b,
+				set(4, 4 , w,
+					create_empty_board())))).
+
 
 other_color(w) ->
 	b;
 other_color(b) ->
 	w.
+
+get_valid_moves(Board, Color) ->
+	w.
+
+%% When color at board(x,y) = Color, Do Something
+when_color(X, Y, Color, Board, DoSomething) ->
+	case board:get(X, Y, Board) of
+		Color ->
+			DoSomething(X, Y, Color, Board);
+		_ ->
+			ok
+	end.
+
+traverse(Color, Board, DoSomething) ->
+	traverse(0 ,0, Color, Board, DoSomething, []).
+
+traverse(X, Y, Color, Board, DoSomething, Results) ->
+	case when_color(X, Y, Color, Board, DoSomething) of
+		Current_Result when (Current_Result =/= ok) ->
+			New_Result = Results ++ [Current_Result];
+		_ ->
+			New_Result = Results
+	end,
+	case {X, Y} of
+		{7, 7} -> New_Result;
+		{X, 7} -> board:traverse(X+1,
+								 0,
+								 Color,
+								 Board,
+								 DoSomething,
+								 New_Result);
+		{X, Y} -> board:traverse(X,
+								 Y+1,
+								 Color,
+								 Board,
+								 DoSomething,
+								 New_Result);
+		_ -> this_wont_happen
+	end.
+
+get_move_directions() ->
+	[{-1, -1}, {-1, 0}, {-1, 1},
+	 {0, 1}, {0, -1},
+	 {1, -1}, {1, 0}, {1, 1}].
+
+%% Find a valid move that can be initiated by coin at From{X,Y}
+%% in Direction{D1,D2}. If a valid move exist, return the coordinates,
+%% else return false
+find_direction_move(From, Direction, Board) ->
+	find_direction_move(From, Direction, Board, {{count,0},
+												 {start, none},
+												 {path, []}
+												}).
+
+find_direction_move(From, Direction, Board, Results) ->
+	{{count, Flip_Count},
+	 {start, Start},
+	 {path, Path}} = Results,
+	{X, Y} = From,
+	{Dx, Dy} = Direction,
+	Color = board:get(X, Y, Board),
+	Other_Color = other_color(Color),
+
+	case {X + Dx, Y + Dy} of
+		{X1, Y1} when (X1 =< 8) and (X1 >= 0) and (Y1 =< 8) and (Y1 >= 0) ->
+			case board:get(X1, Y1, Board) of
+				0 when Flip_Count > 0 ->
+					{{move, {X1, Y1}},
+					 Path};
+				0 when (Flip_Count == 0) ->
+					no_move_here;
+				Other_Color when (Flip_Count == 0) ->
+					find_direction_move({X1, Y1},
+										Direction,
+										Board,
+										{{count, Flip_Count+1},
+										 {start, {X, Y}},
+										 {path, [{X1, Y1}]}
+										});
+				Other_Color when (Flip_Count > 0) ->
+					find_direction_move({X1, Y1},
+										Direction,
+										Board,
+										{{count, Flip_Count+1},
+										 {start, Start},
+										 {path, Path + [{X1, Y1}]}
+										});
+				Color ->
+					no_move_here;
+				_ ->
+					this_wont_happen
+			end;
+		_ ->
+			no_move_here
+	end.
+
+%% For a given position, find all valid moves.
+find_valid_line_moves_from(From, Board) ->
+	lists:filter(fun(X) -> X =/= no_move_here end,
+				 lists:map(fun(X) -> find_direction_move(From, X, Board) end,
+						   get_move_directions())).
+
+%% For a given color(Multiple positions on board), find all valid line moves.
+find_valid_line_moves_for(Color, Board) ->
+	lists:merge(
+	  board:traverse(Color,
+					 Board,
+					 fun(X, Y, C, B) ->
+							 board:find_valid_line_moves_from({X, Y},
+														 B)
+					 end)).
+
+merge_line_moves([], D) ->
+	D;
+merge_line_moves([H|T], D) ->
+	{Move, Path} = H,
+	case dict:is_key(Move, D) of
+		true ->
+			merge_line_moves(T, dict:append_list(Move,Path, D));
+		false ->
+			merge_line_moves(T, dict:store(Move, Path, D))
+	end.
+
+find_valid_moves_for(Color, Board) ->
+	dict:to_list(
+	  merge_line_moves(
+		find_valid_line_moves_for(Color, Board),
+		dict:new())).
+
+
+apply_moves([], Color, Board) ->
+	Board;
+apply_moves([H|T], Color, Board) ->
+	{X, Y} = H,
+	apply_moves(T, board:set(X,Y, Color, Board)).
+
+play({{move, Move}, Color, Flips}, Board) ->
+	All = Flips ++ [Move],
+	apply_moves(All, Color, Board).
+
+
+really_dumb_ai_play(Color, Board) ->
+	apply_moves(lists:nth(1,find_valid_moves_for(Color,Board)), Color, Board).
+
+
+rand_function(X, Y, Color, Board) ->
+	io:format("Color ~p ~p ~p~n",[X, Y, Color]).
