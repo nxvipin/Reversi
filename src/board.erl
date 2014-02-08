e5f8e232ebec0e734a6ce1646a0547efd385c7cf
Vipin Nair <swvist@gmail.com>
2014-02-08 23:44:59 +0530

    Refactoring point positions to tuple and some other changes I should have tracked long back :(

--- src/board.erl
+++ src/board.erl
@@ -8,17 +8,17 @@ create_empty_board() ->
 								   {fixed, true},
 								   {default, 0}])}]).
 
-set(X, Y, Value, Board) ->
+set({X, Y}, Value, Board) ->
 	array:set(X, array:set(Y, Value, array:get(X, Board)), Board).
 
-get(X, Y, Board) ->
+get({X, Y}, Board) ->
 	array:get(Y, array:get(X, Board)).
 
 create_default_board() ->
-	set(3, 3, w,
-		set(3, 4, b,
-			set(4, 3, b,
-				set(4, 4 , w,
+	set({3, 3}, w,
+		set({3, 4}, b,
+			set({4, 3}, b,
+				set({4, 4} , w,
 					create_empty_board())))).
 
 
@@ -27,12 +27,9 @@ other_color(w) ->
 other_color(b) ->
 	w.
 
-get_valid_moves(Board, Color) ->
-	w.
-
 %% When color at board(x,y) = Color, Do Something
-when_color(X, Y, Color, Board, DoSomething) ->
-	case board:get(X, Y, Board) of
+when_color({X, Y}, Color, Board, DoSomething) ->
+	case board:get({X, Y}, Board) of
 		Color ->
 			DoSomething(X, Y, Color, Board);
 		_ ->
@@ -43,7 +40,7 @@ traverse(Color, Board, DoSomething) ->
 	traverse(0 ,0, Color, Board, DoSomething, []).
 
 traverse(X, Y, Color, Board, DoSomething, Results) ->
-	case when_color(X, Y, Color, Board, DoSomething) of
+	case when_color({X, Y}, Color, Board, DoSomething) of
 		Current_Result when (Current_Result =/= ok) ->
 			New_Result = Results ++ [Current_Result];
 		_ ->
@@ -75,23 +72,25 @@ get_move_directions() ->
 %% in Direction{D1,D2}. If a valid move exist, return the coordinates,
 %% else return false
 find_direction_move(From, Direction, Board) ->
-	find_direction_move(From, Direction, Board, {{count,0},
+	Color = board:get(From, Board),
+	find_direction_move(From, Direction, Board, {{start_color, Color},
+												 {count,0},
 												 {start, none},
 												 {path, []}
 												}).
 
 find_direction_move(From, Direction, Board, Results) ->
-	{{count, Flip_Count},
+	{{start_color, Color},
+	 {count, Flip_Count},
 	 {start, Start},
 	 {path, Path}} = Results,
 	{X, Y} = From,
 	{Dx, Dy} = Direction,
-	Color = board:get(X, Y, Board),
 	Other_Color = other_color(Color),
 
 	case {X + Dx, Y + Dy} of
 		{X1, Y1} when (X1 =< 8) and (X1 >= 0) and (Y1 =< 8) and (Y1 >= 0) ->
-			case board:get(X1, Y1, Board) of
+			case board:get({X1, Y1}, Board) of
 				0 when Flip_Count > 0 ->
 					{{move, {X1, Y1}},
 					 Path};
@@ -101,7 +100,8 @@ find_direction_move(From, Direction, Board, Results) ->
 					find_direction_move({X1, Y1},
 										Direction,
 										Board,
-										{{count, Flip_Count+1},
+										{{start_color, Color},
+										 {count, Flip_Count+1},
 										 {start, {X, Y}},
 										 {path, [{X1, Y1}]}
 										});
@@ -109,9 +109,10 @@ find_direction_move(From, Direction, Board, Results) ->
 					find_direction_move({X1, Y1},
 										Direction,
 										Board,
-										{{count, Flip_Count+1},
+										{{start_color, Color},
+										 {count, Flip_Count+1},
 										 {start, Start},
-										 {path, Path + [{X1, Y1}]}
+										 {path, Path ++ [{X1, Y1}]}
 										});
 				Color ->
 					no_move_here;
@@ -133,9 +134,9 @@ find_valid_line_moves_for(Color, Board) ->
 	lists:merge(
 	  board:traverse(Color,
 					 Board,
-					 fun(X, Y, C, B) ->
+					 fun(X, Y, _C, B) ->
 							 board:find_valid_line_moves_from({X, Y},
-														 B)
+															  B)
 					 end)).
 
 merge_line_moves([], D) ->
@@ -156,20 +157,47 @@ find_valid_moves_for(Color, Board) ->
 		dict:new())).
 
 
-apply_moves([], Color, Board) ->
+apply_moves([], _Color, Board) ->
 	Board;
 apply_moves([H|T], Color, Board) ->
 	{X, Y} = H,
-	apply_moves(T, board:set(X,Y, Color, Board)).
+	apply_moves(T, Color, board:set({X, Y}, Color, Board)).
 
-play({{move, Move}, Color, Flips}, Board) ->
+play_move({{move, Move}, Flips}, Color, Board) ->
 	All = Flips ++ [Move],
 	apply_moves(All, Color, Board).
 
+is_valid({X, Y}, Color, Board) ->
+	Valid_Moves = find_valid_moves_for(Color, Board),
+	F = fun(_Fun, {_X,_Y}, []) ->
+			false;
+	   (Fun, {X1,Y1}, [H|T]) ->
+			case H of
+				{{move,{X1,Y1}}, _} ->
+					H;
+				_ ->
+					Fun(Fun, {X1,Y1}, T)
+			end
+		end,
+	F(F, {X,Y}, Valid_Moves).
+
+play({X, Y}, Color, Board) ->
+	Move = is_valid({X, Y}, Color, Board),
+	case Move of
+		{{move, {X, Y}}, _} ->
+			play_move(Move, Color, Board);
+		false ->
+			false
+	end.
+
+display_board(Board) ->
+	B = array:to_list(Board),
+	lists:map(fun (X) ->
+					  io:format("~p~n",[array:to_list(X)]),
+					  ok
+			  end, B),
+	ok.
+
 
 really_dumb_ai_play(Color, Board) ->
-	apply_moves(lists:nth(1,find_valid_moves_for(Color,Board)), Color, Board).
-
-
-rand_function(X, Y, Color, Board) ->
-	io:format("Color ~p ~p ~p~n",[X, Y, Color]).
+   play_move(lists:nth(1,find_valid_moves_for(Color,Board)), Color, Board).
